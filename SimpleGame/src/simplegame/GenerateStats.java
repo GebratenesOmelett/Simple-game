8d3ab2d1343ba584b3e79b5b9208ceaccc0ac904
Aleksy Biela <aleks.302@wp.pl>
2023-05-25 23:39:59 +0200

    Create GenerateStats.java

---
+++ SimpleGame/src/simplegame/GenerateStats.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package simplegame;
+
+/**
+ *
+ * @author Aleksy
+ */
+public class GenerateStats{
+    public int GenerateStatsAttacl(int minAttack, int maxAttack){
+        int randomAttack = (int)Math.floor(Math.random() * (maxAttack - minAttack + 1) + minAttack);
+        return randomAttack;
+    }
+    public int GenerateStatsDefence(int minDefence, int maxDefence){
+        int randomDefence = (int)Math.floor(Math.random() * (maxDefence - minDefence + 1) + minDefence);
+        return randomDefence;
+    }
+    public int GenerateStatsHealth(int minHealth, int maxHealth){
+        int randomHealth = (int)Math.floor(Math.random() * (maxHealth - minHealth + 1) + minHealth);
+        return randomHealth;
+    }
+}
